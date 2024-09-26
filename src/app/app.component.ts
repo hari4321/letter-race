102c117ca485f1334607d73f218537cc0ce4d1e7
hari4321 <hariprasad122333@gmail.com>
2024-09-26 18:19:11 +0530

    Added 3 Screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, RouterLink ,HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'letter-race';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { TypingScreenComponent } from './typing-screen/typing-screen.component';
-import { ResultComponent } from './result/result.component';
+import { HomeComponent } from './components/home/home.component';
+import { TypingScreenComponent } from './components/typing-screen/typing-screen.component';
+import { ResultComponent } from './components/result/result.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
